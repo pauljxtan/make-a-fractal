756b823030df115d3fdf3eb582f682bca40c5558
Paul Tan <pjxtan@gmail.com>
2017-10-01 16:25:10 -0400

    Consolidated to single JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
+var rgbaBlack = 'rgba(0,0,0,1';
+
 $(document).ready(function () {
-    var ctx = document.getElementById("canvas").getContext("2d");
+    var ctx = $('#canvas')[0].getContext("2d");
     var chart = new Chart(ctx, {
         type: 'scatter',
         data: {
             datasets: [
                 //getMockSineDataset()
-                getBarnsleyDataset(10000)
+                getDataset("Barnsley's fern", barnsley(10000))
             ]
         },
         options: {
+            // Disable all hover events
+            events: [],
             title: {
                 display: true,
-                text: 'Testing'
+                text: 'Make a fractal'
             },
-            events: []
+            legend: {
+                display: false
+            },
+            elements: {
+                point: {
+                    radius: 1,
+                    backgroundColor: rgbaBlack,
+                    borderColor: rgbaBlack                }
+            }
         }
     });
 });
@@ -23,10 +35,6 @@ function getDataset(label, data) {
         label: label,
         data: data
     };
-}
-
-function getBarnsleyDataset(iterations) {
-    return getDataset("Barnsley's fern", barnsley(iterations))
 }
 
 function getMockSineDataset() {
@@ -40,5 +48,68 @@ function getMockSineData() {
             x: x,
             y: Math.sin(x)
         });
+    return data;
+}
+
+
+/**************** FRACTALS ****************/
+
+/*
+https://en.wikipedia.org/wiki/Barnsley_fern
+          | 0.00  0.00| |x|   |0.00|
+f1(x,y) = |           | | | + |    |
+          | 0.00  0.16| |y|   |0.00|
+
+          | 0.85  0.04| |x|   |0.00|
+f2(x,y) = |           | | | + |    |
+          |-0.04  0.85| |y|   |1.60|
+
+          | 0.20 -0.26| |x|   |0.00|
+f3(x,y) = |           | | | + |    |
+          | 0.23  0.22| |y|   |1.60|
+
+          |-0.15  0.28| |x|   |0.00|
+f4(x,y) = |           | | | + |    |
+          | 0.26  0.24| |y|   |0.44|
+*/
+function barnsley(iterations) {
+    var data = [];
+
+    // Initialize at origin
+    data.push({x: 0.0, y: 0.0});
+
+    for (var i = 1; i < iterations; i++) {
+        var roll = Math.random();
+        var prev = data[i - 1];
+
+        // 1% probability
+        if (roll < 0.01) {
+            data.push({
+                x: 0.0,
+                y: 0.16 * prev.y
+            });
+        }
+        // 85% probability
+        else if (roll < 0.86) {
+            data.push({
+                x:  0.85 * prev.x + 0.04 * prev.y,
+                y: -0.04 * prev.x + 0.85 * prev.y + 1.6
+            });
+        }
+        // 7% probability
+        else if (roll < 0.93) {
+            data.push({
+                x: 0.20 * prev.x - 0.26 * prev.y,
+                y: 0.23 * prev.x + 0.22 * prev.y + 1.6
+            });
+        }
+        // 7% probability
+        else {
+            data.push({
+                x: -0.15 * prev.x + 0.28 * prev.y,
+                y:  0.26 * prev.x + 0.24 * prev.y + 0.44
+            });
+        }
+    }
     return data;
 }
