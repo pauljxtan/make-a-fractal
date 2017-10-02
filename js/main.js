a27b21eb8e88af2540e985f5edd8ef0dbb3c0486
Paul Tan <pjxtan@gmail.com>
2017-10-02 16:50:01 -0400

    Switching from Chart.js to dygraphs; added an informational blurb

--- js/main.js
+++ js/main.js
@@ -1,56 +1,55 @@
 var rgbaBlack = 'rgba(0,0,0,1';
 
 $(document).ready(function () {
-    var ctx = $('#canvas')[0].getContext("2d");
-    var chart = new Chart(ctx, {
-        type: 'scatter',
-        data: {
-            datasets: [
-                //getMockSineDataset()
-                getDataset("Barnsley's fern", barnsley(10000))
-            ]
-        },
-        options: {
-            // Disable all hover events
-            events: [],
-            title: {
-                display: true,
-                text: 'Make a fractal'
-            },
-            legend: {
-                display: false
-            },
-            elements: {
-                point: {
-                    radius: 1,
-                    backgroundColor: rgbaBlack,
-                    borderColor: rgbaBlack                }
-            }
+    var data = barnsley(50000);
+    // var data = getMockSineData(32);
+
+    // Save min and max on each axis for resetting zoom
+    var xMinMax = minMaxByDim(data, 0);
+    var yMinMax = minMaxByDim(data, 1);
+    var fullDateWindow = [xMinMax.min, xMinMax.max];
+    var fullValueRange = [yMinMax.min, yMinMax.max];
+
+    var graph = new Dygraph(
+        document.getElementById('graph'),
+        data,
+        {
+            drawPoints: true,
+            strokeWidth: 0.0,
+            height: 500,
+            width: 500,
+            dateWindow: fullDateWindow,
+            valueRange: fullValueRange,
+            legend: 'never',
+
         }
+    );
+
+    $('#unzoom-button').click(function () {
+        graph.updateOptions({
+            dateWindow: fullDateWindow,
+            valueRange: fullValueRange
+        });
     });
 });
 
-function getDataset(label, data) {
-    return {
-        label: label,
-        data: data
-    };
+
+function minMaxByDim(multiDimArray, dim) {
+    var dimArray = multiDimArray.map(function (elem) {
+        return elem[dim]
+    });
+    var min = Math.min.apply(null, dimArray);
+    var max = Math.max.apply(null, dimArray);
+    return {min: min, max: max};
 }
 
-function getMockSineDataset() {
-    return getDataset("Sine", getMockSineData());
-}
-
-function getMockSineData() {
+function getMockSineData(points) {
     var data = [];
-    for (var x = -Math.PI; x < Math.PI; x += 0.1)
-        data.push({
-            x: x,
-            y: Math.sin(x)
-        });
+    var step = 2.0 * Math.PI / points;
+    for (var x = -Math.PI; x < Math.PI; x += step)
+        data.push([x, Math.sin(x)]);
     return data;
 }
-
 
 /**************** FRACTALS ****************/
 
@@ -76,7 +75,7 @@ function barnsley(iterations) {
     var data = [];
 
     // Initialize at origin
-    data.push({x: 0.0, y: 0.0});
+    data.push([0.0, 0.0]);
 
     for (var i = 1; i < iterations; i++) {
         var roll = Math.random();
@@ -84,31 +83,31 @@ function barnsley(iterations) {
 
         // 1% probability
         if (roll < 0.01) {
-            data.push({
-                x: 0.0,
-                y: 0.16 * prev.y
-            });
+            data.push([
+                0.0,
+                0.16 * prev[1]
+            ]);
         }
         // 85% probability
         else if (roll < 0.86) {
-            data.push({
-                x:  0.85 * prev.x + 0.04 * prev.y,
-                y: -0.04 * prev.x + 0.85 * prev.y + 1.6
-            });
+            data.push([
+                +0.85 * prev[0] + 0.04 * prev[1],
+                -0.04 * prev[0] + 0.85 * prev[1] + 1.6
+            ]);
         }
         // 7% probability
         else if (roll < 0.93) {
-            data.push({
-                x: 0.20 * prev.x - 0.26 * prev.y,
-                y: 0.23 * prev.x + 0.22 * prev.y + 1.6
-            });
+            data.push([
+                0.20 * prev[0] - 0.26 * prev[1],
+                0.23 * prev[0] + 0.22 * prev[1] + 1.6
+            ]);
         }
         // 7% probability
         else {
-            data.push({
-                x: -0.15 * prev.x + 0.28 * prev.y,
-                y:  0.26 * prev.x + 0.24 * prev.y + 0.44
-            });
+            data.push([
+                -0.15 * prev[0] + 0.28 * prev[1],
+                +0.26 * prev[0] + 0.24 * prev[1] + 0.44
+            ]);
         }
     }
     return data;
